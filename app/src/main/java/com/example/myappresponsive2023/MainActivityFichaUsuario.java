af31877a208ae3536729a03bd7bc726988730bad
kevinbeles1999 <kbeltrane@uteq.edu.ec>
2023-07-20 17:20:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myappresponsive2023/MainActivityFichaUsuario.java
@@ -0,0 +1,14 @@
+package com.example.myappresponsive2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityFichaUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ficha_usuario);
+    }
+}
